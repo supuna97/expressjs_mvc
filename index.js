4ecb653039a7ed4571ee867ea67e012eedc960a5
supuna97 <supunnanayakkara01@gmail.com>
2023-10-06 20:33:02 +0530

    Add cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
+
+app.use(cors()); // Allow all origins (not recommended for production)
 
 const route = require("./routes/api_v1");
 app.use("/v1", route);
