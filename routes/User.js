ce187e72f78ada59d56e2c42e9bb9b5547c5eae4
supuna97 <supunnanayakkara01@gmail.com>
2023-10-06 19:48:51 +0530

    Update api routes

--- routes/User.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const userController = require("../controllers/UserController");
-
-router.get("/", userController.get);
-router.post("/create", userController.post);
-
-module.exports = router;
